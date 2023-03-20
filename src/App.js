26178ce4197cb69b4a10b098a3dfd95545e23463
chloehmc <122628488+chloehmc@users.noreply.github.com>
2023-03-20 13:51:21 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-container">
+     
+  
         <h1>React Weather App</h1>
-      </header>
+        
+          <button type="button" className="btn btn-primary">
+          Primary
+        </button>
 
+       
+      </div>
       <footer>
-        <small>
-          <a
-            href="https://github.com/chloehmc/react_weather_app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            Open source code
-          </a>{" "}
-          by Chloe Comstock
-        </small>
-      </footer>
+          <small>
+            <a
+              href="https://github.com/chloehmc/react_weather_app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Open source code
+            </a>{" "}
+            by Chloe Comstock
+          </small>
+        </footer>
     </div>
   );
 }
