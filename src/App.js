f82562485b67bf22cdc65767a3154823882a7517
chloehmc <122628488+chloehmc@users.noreply.github.com>
2023-03-20 13:16:29 +0100

    added github project link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <h1>React Weather App</h1>
-      
       </header>
 
-   
-      
+      <footer>
+        <small>
+          <a
+            href="https://github.com/chloehmc/react_weather_app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open source code
+          </a>{" "}
+          by Chloe Comstock
+        </small>
+      </footer>
     </div>
   );
 }
