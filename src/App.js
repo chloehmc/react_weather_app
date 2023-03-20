6764dab91bafbf4c22adc31e18027bd7e6366552
chloehmc <122628488+chloehmc@users.noreply.github.com>
2023-03-20 14:12:21 +0100

    created Weather component for HTML and CSS design

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="App-container">
-     
-  
-        <h1>React Weather App</h1>
-        
-          <button type="button" className="btn btn-primary">
-          Primary
-        </button>
+      
 
-       
+        <Weather />
+
       </div>
       <footer>
-          <small>
-            <a
-              href="https://github.com/chloehmc/react_weather_app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {" "}
-              Open source code
-            </a>{" "}
-            by Chloe Comstock
-          </small>
-        </footer>
+        <small>
+          <a
+            href="https://github.com/chloehmc/react_weather_app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open source code
+          </a>{" "}
+          by Chloe Comstock
+        </small>
+      </footer>
     </div>
   );
 }
-
-export default App;
