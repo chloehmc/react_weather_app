d172c9ea6527569fea6c60a134ebe7a1430a2eb4
chloehmc <122628488+chloehmc@users.noreply.github.com>
2023-03-20 15:10:58 +0100

    Added basic weather HTML

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,40 @@
 import React from "react";
-import "./Weather.css"
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1>React Weather App</h1>
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input placeholder="Enter a city" className="form-control" />
+                  </div>
+                  <div className="col-3">
+                  <input type="submit" value="Search" className="btn btn-primary" />
+                  </div>
+        </div>
+      </form>
+      <h1>Amsterdam</h1>
+      <h4>Monday, March 20, 00:00</h4>
+      <h5>Rainy</h5>
+      <div className="row">
+        <div className="col-6">
+          {" "}
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+            alt="icon"
+          ></img>{" "}
+          0 °C
+        </div>
+
+        <div className="col-6">
+          <ul>
+            <li>Feels like: 0 °C</li>
+            <li>Humidity: 0 % </li>
+            <li>Wind: 0km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
