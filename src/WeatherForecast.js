1199cc032c234de654e770830a756f2cc666e916
chloehmc <122628488+chloehmc@users.noreply.github.com>
2023-04-03 12:20:58 +0200

    Trying to update forecast on city change

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,22 +5,23 @@ import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
+  let [forecastData, setForecastData] = useState(null);
 
   useEffect(() => {
     setLoaded(false);
   }, [props.city]);
 
   function handleResponse(response) {
+
+    setForecastData(response.data.daily);
     setLoaded(true);
-    setForecast(response.data.daily);
   }
 
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function(dailyForecast, index) {
+          {forecastData.map(function(dailyForecast, index) {
             if (index < 5) {
               return (
                 <div className="col" key={index}>
@@ -34,7 +35,8 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=London&key=06740ta7fb3c0b6bo8bced4f1a40e926&units=metric`;
+    let apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=London&key=06740ta7fb3c0b6bo8bced4f1a40e926&units=metric`;
+      //`https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=06740ta7fb3c0b6bo8bced4f1a40e926&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return null;
